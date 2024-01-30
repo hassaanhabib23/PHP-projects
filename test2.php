f53f0c7aebe24ef83ffbd676a8b21fb31feb2d78
Hassaan Habib <hassaanhabib162>
2024-01-30 23:52:03 +0500

    first commit

---
+++ test2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    div{
+        width: 300px;
+  margin: auto;
+  border: 1px solid red;
+    }
+</style>
+<body>
+    <div id="style">
+        <p>what is your name</p>
+    </div>
+</body>
+</html>
